562ef83eb58de8f6e88054f56d3cf413703e6ff4
Unwatch <hjzqhd@qq.com>
2018-12-09 16:47:02 +0800

    添加搜狗

--- RankHelper/搜索引擎/Sogou.cs
+++ RankHelper/搜索引擎/Sogou.cs
@@ -12,9 +12,272 @@ namespace RankHelper
 {
     public class Sogou : WebBase
     {
-        public Sogou(WebForm webForm, string strPageurl, string strhtml):base(webForm, strPageurl, strhtml)
+        public Sogou(WebForm webForm, string strPageurl, string strhtml) : base(webForm, strPageurl, strhtml)
         {
+            nPos_x = 30;
+            nPos_y = 80;
             webForm.webBrowser_new.Navigate("www.sogou.com");
         }
+
+        public override void webBrowser_DocumentCompleted_Search(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.ToString().Contains("sogou"))
+            {
+                webForm.currentTask.webState = EWebbrowserState.none;
+                webForm.textBox_url.Text = e.Url.ToString();
+                HtmlElement ele_search;
+                if (webForm.currentTask.webBrowser == eWebBrowser.IE || webForm.currentTask.webBrowser == eWebBrowser.Chrome || webForm.currentTask.webBrowser == eWebBrowser.Qihu || webForm.currentTask.webBrowser == eWebBrowser.Sogou || webForm.currentTask.webBrowser == eWebBrowser.QQ || webForm.currentTask.webBrowser == eWebBrowser.maxthon || webForm.currentTask.webBrowser == eWebBrowser.theworld)
+                {
+                    ele_search = webForm.webBrowser_new.Document.GetElementById("query");
+                }
+                else//移动端
+                {
+                    ele_search = webForm.webBrowser_new.Document.GetElementById("query");
+                }
+
+                if (ele_search == null)
+                {
+                    webForm.currentTask.webState = EWebbrowserState.Search;
+                    webForm.webBrowser_new.Refresh();
+                    return;
+                }
+
+                KeyUtils.SetCursorPos(500, 280);
+
+                KeyUtils.MouseLBUTTON();
+                this.taskIntervalTimer.Start();
+                KeyUtils.Copy(webForm.currentTask.strKeyword);
+
+                Sleep(3000);
+                HtmlElement ele_submit;
+
+                if (webForm.currentTask.webBrowser == eWebBrowser.IE || webForm.currentTask.webBrowser == eWebBrowser.Chrome || webForm.currentTask.webBrowser == eWebBrowser.Qihu || webForm.currentTask.webBrowser == eWebBrowser.Sogou || webForm.currentTask.webBrowser == eWebBrowser.QQ || webForm.currentTask.webBrowser == eWebBrowser.maxthon || webForm.currentTask.webBrowser == eWebBrowser.theworld)
+                {
+                    ele_submit = webForm.webBrowser_new.Document.GetElementById("stb");
+                }
+                else
+                {
+                    ele_submit = webForm.webBrowser_new.Document.GetElementById("stb");
+                }
+
+                if (ele_submit == null)
+                {
+                    webForm.currentTask.webState = EWebbrowserState.Search;
+                    webForm.webBrowser_new.Refresh();
+                    return;
+                }
+
+                KeyUtils.SetCursorPos(760, 280);
+                KeyUtils.MouseLBUTTON();
+                this.taskIntervalTimer.Start();
+
+                if (webForm.currentTask == null)
+                {
+                    return;
+                }
+                webForm.currentTask.webState = EWebbrowserState.Search;
+                webForm.ShowTask(new AppEventArgs() { message_string = string.Format("开始搜索关键词,任务{0}", webForm.currentTask.nID) });
+            }
+        }
+
+        public override void webBrowser_DocumentCompleted_SearchSite(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //if (e.Url.ToString().Equals("http://www.so.com/") || e.Url.ToString().Equals("https://www.so.com/"))
+            if (!e.Url.ToString().Contains("www.sogou.com/web?query="))
+            {
+                return;
+            }
+
+            nItem = 0;
+
+            if (e.Url.ToString().Contains("sogou"))
+            {
+                webForm.currentTask.webState = EWebbrowserState.none;
+                webForm.textBox_url.Text = e.Url.ToString();
+                webForm.ShowTask(new AppEventArgs() { message_string = string.Format("开始查找网站,当前页码{0},任务{1}", nPageIndex, webForm.currentTask.nID) });
+                Sleep(3000);
+
+                //List<tagSearchResult> listSearch = new List<tagSearchResult>();
+                //搜索项目
+                //HtmlElement ele_search;
+                HtmlElementCollection divCol = webForm.webBrowser_new.Document.GetElementsByTagName("div");
+
+                if (divCol != null)
+                {
+                    for (int i = 0; i < divCol.Count; i++)//1~10项
+                    {
+                        if (divCol[i].GetAttribute("className") == null)
+                        {
+                            continue;
+                        }
+
+                        if (divCol[i].GetAttribute("className") == "results")
+                        {
+                            HtmlElementCollection eleCol = divCol[i].Children;
+
+                            if (eleCol != null)
+                            {
+                                for (int j = 0; j < eleCol.Count; j++)//1~10项
+                                {
+                                    if (eleCol[j].GetAttribute("className") == null)
+                                    {
+                                        continue;
+                                    }
+
+                                    if (eleCol[j].GetAttribute("className") == "rb"|| eleCol[j].GetAttribute("className") == "vrwrap")
+                                    {
+                                        nItem++;
+                                        if (!webForm.currentTask.strTitle.Trim().Equals("") && !webForm.currentTask.strPageUrl.Trim().Equals(""))
+                                        {
+                                            if (eleCol[j].InnerText.Contains(webForm.currentTask.strTitle) || eleCol[j].InnerText.Contains(webForm.currentTask.strPageUrl))
+                                            {
+                                                webForm.ShowTask(new AppEventArgs() { message_string = string.Format("查找到符合的网站,当前页码{0},任务{1}", nPageIndex, webForm.currentTask.nID) });
+                                                webForm.currentTask.webState = EWebbrowserState.AccessSite;
+                                                Sleep(3000);
+                                                Point point_ele = GetPoint(eleCol[j]);
+                                                int nPos_y = 60;
+
+                                                webForm.webBrowser_new.Document.Window.ScrollTo(0, point_ele.Y - nPos_y);
+                                                int top2 = webForm.webBrowser_new.Document.GetElementsByTagName("HTML")[0].ScrollTop;//滚动条垂直位置
+                                                                                                                                     //KeyUtils.SetCursorPos(point_ele.X + nPos_x - 900, point_ele.Y + nPos_y - top2);
+                                                KeyUtils.SetCursorPos(point_ele.X + nPos_x, point_ele.Y + nPos_y - top2);
+                                                Sleep(3000);
+                                                webForm.currentTask.webState = EWebbrowserState.AccessSite;
+                                                KeyUtils.MouseLBUTTON();
+                                                this.taskIntervalTimer.Start();
+
+                                                return;
+                                            }
+                                        }
+                                        else if (!webForm.currentTask.strTitle.Trim().Equals(""))
+                                        {
+                                            if (eleCol[j].InnerText.Contains(webForm.currentTask.strTitle))
+                                            {
+                                                webForm.ShowTask(new AppEventArgs() { message_string = string.Format("查找到符合的网站,当前页码{0},任务{1}", nPageIndex, webForm.currentTask.nID) });
+                                                webForm.currentTask.webState = EWebbrowserState.AccessSite;
+                                                Sleep(3000);
+                                                Point point_ele = GetPoint(eleCol[j]);
+                                                int nPos_y = 60;
+
+                                                webForm.webBrowser_new.Document.Window.ScrollTo(0, point_ele.Y - nPos_y);
+                                                int top2 = webForm.webBrowser_new.Document.GetElementsByTagName("HTML")[0].ScrollTop;//滚动条垂直位置
+                                                                                                                                     //KeyUtils.SetCursorPos(point_ele.X + nPos_x - 900, point_ele.Y + nPos_y - top2);
+                                                KeyUtils.SetCursorPos(point_ele.X + nPos_x, point_ele.Y + nPos_y - top2);
+                                                Sleep(3000);
+                                                webForm.currentTask.webState = EWebbrowserState.AccessSite;
+                                                KeyUtils.MouseLBUTTON();
+                                                this.taskIntervalTimer.Start();
+
+                                                return;
+                                            }
+                                        }
+                                        else if (!webForm.currentTask.strPageUrl.Trim().Equals(""))
+                                        {
+                                            webForm.ShowTask(new AppEventArgs() { message_string = string.Format("查找到符合的网站,当前页码{0},任务{1}", nPageIndex, webForm.currentTask.nID) });
+                                            webForm.currentTask.webState = EWebbrowserState.AccessSite;
+                                            Sleep(3000);
+                                            Point point_ele = GetPoint(eleCol[j]);
+                                            int nPos_y = 60;
+
+                                            webForm.webBrowser_new.Document.Window.ScrollTo(0, point_ele.Y - nPos_y);
+                                            int top2 = webForm.webBrowser_new.Document.GetElementsByTagName("HTML")[0].ScrollTop;//滚动条垂直位置
+                                                                                                                                 //KeyUtils.SetCursorPos(point_ele.X + nPos_x - 900, point_ele.Y + nPos_y - top2);
+                                            KeyUtils.SetCursorPos(point_ele.X + nPos_x, point_ele.Y + nPos_y - top2);
+                                            Sleep(3000);
+                                            webForm.currentTask.webState = EWebbrowserState.AccessSite;
+                                            KeyUtils.MouseLBUTTON();
+                                            this.taskIntervalTimer.Start();
+
+                                            return;
+                                        }
+                                    }
+                                }
+
+                                //当前搜索页未找到
+                                //if (nItem == -1)
+                                {
+                                    webForm.ShowTask(new AppEventArgs() { message_string = string.Format("没有找到符合的网站,开始查找下一页，当前页码{0},任务{1}", nPageIndex, webForm.currentTask.nID) });
+                                    GetNextPageurl();
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void webBrowser_DocumentCompleted_AccessSite(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.ToString().Equals("http://www.so.com/") || e.Url.ToString().Equals("https://www.so.com/"))
+            {
+                return;
+            }
+            //if (e.Url.ToString().Contains("so"))
+            //{
+            //    return;
+            //}
+            webForm.textBox_url.Text = e.Url.ToString();
+            webForm.currentTask.webState = EWebbrowserState.none;
+            webForm.textBox_url.Text = e.Url.ToString();
+            webForm.ShowTask(new AppEventArgs() { message_string = string.Format("进入网站,当前页码{0},任务{1}", 1, webForm.currentTask.nID) });
+
+            Sleep(5000);
+            switch (webForm.currentTask.pageAccessType)
+            {
+                case ePageAccessType.None:
+                    {
+                        EndTask(true);
+                    }
+                    break;
+                case ePageAccessType.Rand:
+                    {
+                        EndTask(true);
+                    }
+                    break;
+                case ePageAccessType.Appoint:
+                    {
+                        EndTask(true);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public override void GetNextPageurl()
+        {
+            HtmlElement ele_page;
+            ele_page = webForm.webBrowser_new.Document.GetElementById("pagebar_container");
+            if (ele_page == null)
+            {
+                webForm.ShowTask(new AppEventArgs() { message_string = string.Format("只有一页，结束任务，当前页码{0},任务{1}", nPageIndex, webForm.currentTask.nID) });
+                EndTask(false);
+            }
+            nPageIndex += 1;
+            HtmlElementCollection eleCol = ele_page.GetElementsByTagName("a");
+            foreach (HtmlElement element in eleCol)
+            {
+                if (element.InnerText == nPageIndex.ToString())
+                {
+                    webForm.currentTask.webState = EWebbrowserState.SearchSite;
+                    Point point_ele = GetPoint(element);
+                    webForm.webBrowser_new.Document.Window.ScrollTo(0, point_ele.Y);
+                    int top = webForm.webBrowser_new.Document.GetElementsByTagName("HTML")[0].ScrollTop;//滚动条垂直位置
+                    KeyUtils.SetCursorPos(point_ele.X + nPos_x, point_ele.Y + nPos_y - top);
+                    Sleep(3000);
+                    KeyUtils.MouseLBUTTON();
+                    this.taskIntervalTimer.Start();
+
+                    return;
+                }
+            }
+
+            webForm.ShowTask(new AppEventArgs() { message_string = string.Format("已经是最后一页，结束任务，当前页码{0},任务{1}", nPageIndex, webForm.currentTask.nID) });
+            Sleep(5000);
+            EndTask(false);
+        }
+
     }
 }
